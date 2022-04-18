a599dc5b9597bc165b9f73e325f6caa76101a92e
JC-78 <43752338+JC-78@users.noreply.github.com>
2022-04-18 11:29:20 -0400

    jquery

---
+++ interaction.js
@@ -0,0 +1,8 @@
+$(document).ready( function() {
+    $("#workexperience").click( function() { windowlocation.href ="https://jc-78.github.io/web/#workexperience"; });
+    $("#researchexperience").click( function() { windowlocation.href ="https://jc-78.github.io/web/#researchexperience"; });
+    $("#personalprojects").click( function() { windowlocation.href = "https://jc-78.github.io/web/#personalprojects"; });
+    $("#bio").click( function() { windowlocation.href = "https://jc-78.github.io/web/#bio"; });
+    $("#contacts").click( function() { windowlocation.href = "https://jc-78.github.io/web/#contacts"; });
+  });
+
